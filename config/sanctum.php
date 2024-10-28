1ab058cae92be114106504af839c5612a0c85270
Gurpreet <gurpreetkait.codes@gmail.com>
2024-10-29 04:04:07 +0530

    modified sanctum

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,9 +17,10 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'dailyexpensetracker.in,www.dailyexpensetracker.in,api.dailyexpensetracker.in',
-        env('APP_URL') ? ',' . parse_url(env('APP_URL'), PHP_URL_HOST) : ''
+        'localhost,localhost:5173,127.0.0.1,127.0.0.1:8000,::1',
+        Sanctum::currentApplicationUrlWithPort()
     ))),
+
     /*
     |--------------------------------------------------------------------------
     | Sanctum Guards
