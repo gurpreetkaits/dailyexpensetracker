b83c89a82b60158722e3d689b7b44dca24eb0de8
Gurpreet <gurpreetkait.codes@gmail.com>
2024-11-02 01:14:31 +0530

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <!-- SEO Meta Tags -->
     <title>Daily Expense Tracker - Manage Your Money Effectively | dailyexpensetracker.in</title>
     <meta name="description"
