e9987f7cb38129f0142f0fc68430748e4b291eed
Gurpreet <gurpreetkait.codes@gmail.com>
2024-11-04 18:23:53 +0530

    Fix

--- database/migrations/2024_11_04_125029_modify_users_table.php
+++ database/migrations/2024_11_04_125029_modify_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('avatar')->change();
+            $table->text('avatar')->nullable()->change();
         });
     }
 
