b4aff485b20c0ef86dfe0d86441bb8679dc11df8
Gurpreet Kait <gurpreetkait.codes@gmail.com>
2024-12-27 14:16:08 +0530

    lets test out again

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
+        // laravel({
+        //     input: 'resources/js/app.js',
+        //     refresh: true,
+        // }),
         vue({
             template: {
                 transformAssetUrls: {
