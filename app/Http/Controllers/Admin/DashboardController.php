439d0a41d69318df8d7466631464c12334f26529
Gurpreet Kait <gurpreetkait.codes@gmail.com>
2025-05-05 04:20:58 +0530

    fix

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
