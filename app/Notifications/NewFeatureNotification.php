fde412ec1843bc519514de271b115ecdafb38683
Gurpreet Kait <gurpreetkait.codes@gmail.com>
2025-05-10 22:29:57 +0530

    queued

--- app/Notifications/NewFeatureNotification.php
+++ app/Notifications/NewFeatureNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class NewFeatureNotification extends Notification
+class NewFeatureNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
