48e9c31cf5b0ee065d097d819bbc6ed4e187c23d
Gurpreet Kait <gurpreetkait.codes@gmail.com>
2025-05-16 11:22:51 +0530

    added two icons that weere not visible

--- frontend/src/mixins/iconMixin.js
+++ frontend/src/mixins/iconMixin.js
@@ -4,20 +4,20 @@ import {
   Citrus, ShoppingBag, House, Receipt, Clapperboard, Plane, Contact,
   Cross, ShoppingCart, Book, BriefcaseBusiness, BadgeDollarSign,
   Dumbbell, Sparkle, SearchIcon, CircleDot, CircleX, TrendingUp, TrendingDown, 
-  ArrowUpCircle, ArrowDownCircle, PiggyBank, CalendarClock, RepeatIcon,
+  ArrowUpCircle, ArrowDownCircle, PiggyBank, CalendarClock, RepeatIcon,Briefcase,
   Utensils, Coffee, Heart, GraduationCap, Music, GamepadIcon, Phone, Wifi, Zap,
-  Droplet, Pill, Stethoscope, Bus, Train, Ship, CreditCard, BanknoteIcon
+  Droplet, Pill, Stethoscope, Bus, Train, Ship, CreditCard, BanknoteIcon,Home
 } from 'lucide-vue-next'
 
 export const iconMixin = {
   components: {
     Calendar, Trash2, Plus, Car, ReceiptIcon, Video, BriefcaseMedical, Gift, Circle, CircleEllipsis, 
     Pizza, CircleDollarSign, HandCoins, Wallet, ChartCandlestick, Landmark,
-    Citrus, ShoppingBag, House, Receipt, Clapperboard, Plane, Contact,
+    Citrus, ShoppingBag, House, Receipt, Clapperboard, Plane, Contact,Briefcase,
     Cross, ShoppingCart, Book, BriefcaseBusiness, BadgeDollarSign,
     Dumbbell, Sparkle, SearchIcon, CircleDot, CircleX, TrendingUp, TrendingDown, 
     ArrowUpCircle, ArrowDownCircle, PiggyBank, CalendarClock, RepeatIcon,
     Utensils, Coffee, Heart, GraduationCap, Music, GamepadIcon, Phone, Wifi, Zap,
-    Droplet, Pill, Stethoscope, Bus, Train, Ship, CreditCard, BanknoteIcon
+    Droplet, Pill, Stethoscope, Bus, Train, Ship, CreditCard, BanknoteIcon,Home
   }
 } 
