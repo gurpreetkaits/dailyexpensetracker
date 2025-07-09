046f6243c77d3ffa2eeeabae9a9fce410eb5d981
Gurpreet Kait <gurpreetkait.codes@gmail.com>
2025-07-09 12:05:02 +0530

    modal sould be large on prodc as well

--- frontend/src/components/Global/GlobalModal.vue
+++ frontend/src/components/Global/GlobalModal.vue
@@ -47,7 +47,7 @@ export default {
     size: {
       type: String,
       default: 'max-w-md',
-      validator: (value) => ['max-w-sm', 'max-w-md', 'max-w-lg', 'max-w-xl', 'max-w-2xl'].includes(value)
+      validator: (value) => ['max-w-sm', 'max-w-md', 'max-w-lg', 'max-w-xl', 'max-w-2xl', 'max-w-3xl', 'max-w-4xl', 'max-w-5xl', 'max-w-6xl', 'max-w-7xl'].includes(value)
     },
     closeOnBackdrop: {
       type: Boolean,
